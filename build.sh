a6bf121d3c3b4a1951a420bdd687304e9612018a
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2021-12-04 09:43:34 +0100

    add release function

--- build.sh
+++ build.sh
@@ -23,6 +23,17 @@ function _build() {
     sha256sum output/${CONFIG}.tar.gz > output/${CONFIG}.tar.gz.sha256
 }
 
+function _release() {
+    hub release create -m v2021.12.01 \
+           -a output/aarch64-rpi3-linux-gnu.tar.gz \
+           -a output/aarch64-rpi3-linux-gnu.tar.gz.sha256 \
+           -a output/arm-cortex_a8-linux-gnueabihf.tar.gz \
+           -a output/arm-cortex_a8-linux-gnueabihf.tar.gz.sha256 \
+           -a output/armv6-rpi-linux-gnueabihf.tar.gz \
+           -a output/armv6-rpi-linux-gnueabihf.tar.gz.sha256 \
+           v2021.12.01
+}
+
 _build aarch64-rpi3-linux-gnu
 _build arm-cortex_a8-linux-gnueabihf
 _build armv6-rpi-linux-gnueabihf
