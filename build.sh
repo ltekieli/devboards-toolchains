e232b8d6a78ac29c6cfb5ed1728a8621844cb166
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2022-05-30 17:43:02 +0200

    add x86_64 toolchain

--- build.sh
+++ build.sh
@@ -6,14 +6,16 @@ source ~/.ct-ng-$(cat ./.ct-ng-version)/activate
 mkdir -p output/downloads
 export CT_PREFIX=${PWD}/output
 
+TAG=2022.05.01
+
 function _build() {
     CONFIG=$1
     DEFCONFIG="${CONFIG}_defconfig" ct-ng defconfig
     ct-ng -j$(nproc) build
-    
+
     tar -c \
         --sort=name \
-        --mtime="2021-12-01" \
+        --mtime="${TAG}" \
         --owner=0 \
         --group=0 \
         --numeric-owner \
@@ -24,16 +26,17 @@ function _build() {
 }
 
 function _release() {
-    hub release create -m v2021.12.01 \
+    hub release create -m "v${TAG}" \
            -a output/aarch64-rpi3-linux-gnu.tar.gz \
            -a output/aarch64-rpi3-linux-gnu.tar.gz.sha256 \
            -a output/arm-cortex_a8-linux-gnueabihf.tar.gz \
            -a output/arm-cortex_a8-linux-gnueabihf.tar.gz.sha256 \
            -a output/armv6-rpi-linux-gnueabihf.tar.gz \
            -a output/armv6-rpi-linux-gnueabihf.tar.gz.sha256 \
-           v2021.12.01
+           "v${TAG}"
 }
 
 _build aarch64-rpi3-linux-gnu
 _build arm-cortex_a8-linux-gnueabihf
 _build armv6-rpi-linux-gnueabihf
+_build x86_64-unknown-linux-gnu
